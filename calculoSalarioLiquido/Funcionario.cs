beb8b8371bb842ba2c02227dcb0e00385c51ecd0
Caio Brdaric <caiobrdaric@gmail.com>
2022-04-24 01:34:32 -0300

    Alterado nome da pasta

---
+++ calculoSalarioLiquido/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculoSalarioLiquido
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+        public int Rg  { get; set; }
+        public int Cpf { get; set; }
+        public int Estado { get; set; }
+        public float Salario { get; set; }
+    }
+}
---
+++ calculoSalarioLiquido/Program.cs
@@ -0,0 +1,35 @@
+﻿    using calculoSalarioLiquido;
+
+    double salario, aliquota, salLiq;
+    Funcionario funcionario = new Funcionario();
+    Console.WriteLine ("Digite o salário do funcionario:");
+    salario = Convert.ToDouble(Console.ReadLine());
+    salLiq = 0;
+    
+
+if (salario <= 1903.38)
+{
+        salLiq = salario;
+    }
+    if (salario >= 1903.39 && salario <= 2826.65)
+    {
+        aliquota = 0.925;
+        salLiq = (salario * aliquota) - 142.80;
+    }
+    if (salario >= 2826.66 && salario <= 3751.06)
+    {
+        aliquota = 0.85;
+        salLiq = (salario * aliquota) - 354.80;
+    }
+    if (salario >= 3751.07 && salario <= 4664.68)
+    {
+        aliquota = 0.775;
+        salLiq = (salario * aliquota) - 636.13;
+    }
+    if (salario >= 4664.69)
+    {
+        aliquota = 0.725;
+        salLiq = (salario * aliquota) - 869.36;
+    }
+
+    Console.WriteLine($"Salário liquido de: {Math.Round(salLiq, 2)}");
