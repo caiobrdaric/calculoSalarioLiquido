1b1e11d5aefafbc1b3e3ffb8a41c6392b43a71b5
Caio Brdaric <caiobrdaric@gmail.com>
2022-04-25 23:58:43 -0300

    Iniciando as listas

--- calculoSalarioLiquido/Program.cs
+++ calculoSalarioLiquido/Program.cs
@@ -5,31 +5,29 @@
     Console.WriteLine ("Digite o salário do funcionario:");
     salario = Convert.ToDouble(Console.ReadLine());
     salLiq = 0;
-    
 
 if (salario <= 1903.38)
 {
-        salLiq = salario;
-    }
-    if (salario >= 1903.39 && salario <= 2826.65)
+    salLiq = salario;
+}
+if (salario >= 1903.39 && salario <= 2826.65)
     {
         aliquota = 0.925;
         salLiq = (salario * aliquota) - 142.80;
     }
-    if (salario >= 2826.66 && salario <= 3751.06)
+if (salario >= 2826.66 && salario <= 3751.06)
     {
         aliquota = 0.85;
         salLiq = (salario * aliquota) - 354.80;
     }
-    if (salario >= 3751.07 && salario <= 4664.68)
+if (salario >= 3751.07 && salario <= 4664.68)
     {
         aliquota = 0.775;
         salLiq = (salario * aliquota) - 636.13;
     }
-    if (salario >= 4664.69)
+if (salario >= 4664.69)
     {
         aliquota = 0.725;
         salLiq = (salario * aliquota) - 869.36;
     }
-
-    Console.WriteLine($"Salário liquido de: {Math.Round(salLiq, 2)}");
+Console.WriteLine($"Salário liquido de: {Math.Round(salLiq, 2)}");
